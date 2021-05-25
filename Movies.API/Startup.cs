82782333408045d03cfad4c86e8e4d317d400f8f
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-25 18:10:57 +0200

    Added protection to the movies API.

--- Movies.API/Startup.cs
+++ Movies.API/Startup.cs
@@ -38,6 +38,10 @@ namespace Movies.API
           ValidateAudience = false
         };
       });
+
+      services.AddAuthorization(options => {
+        options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient"));
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
