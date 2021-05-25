6a0865fe1a26b2c9cb9d435ca58ea7ee8cb9e022
Mikael Storman Gustavsson <miga02@gmail.com>
2021-05-25 18:45:38 +0200

    Added client app.

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+  public class MovieApiService : IMovieApiService
+  {
+    public async Task<IEnumerable<Movie>> GetMovies()
+    {
+      var moviesList = new List<Movie>();
+      moviesList.Add(new Movie
+      {
+        Id = 1,
+        Genre = "Classics",
+        Title = "Moste",
+        Rating = "9.2",
+        ImageUrl = "images/src",
+        ReleaseDate = DateTime.Now,
+        Owner = "micke"
+      });
+
+      return await Task.FromResult(moviesList);
+    }
+
+    public Task<Movie> CreateMovie(Movie movie)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task DeleteMovie(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Movie> GetMovie(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Movie> UpdateMovie(Movie movie)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
